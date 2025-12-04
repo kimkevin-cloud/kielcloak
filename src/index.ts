adbe20757ab9034ed602a427167fa3ff2ac3da84
@tomlonca <tomas_esteban.loncarica@stud.htwk-leipzig.de>
2025-12-04 18:17:45 +0100

    FE-BE Kommunikation läuft dank Einstellungsänderungen am BE. Relates to #3

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@
 
 import express from "express";
 import type { Request, Response } from "express";
-const { getSessionFromStorage, Session } = require("@inrupt/solid-client-authn-node");
-import dotenv from "dotenv";  // Dotenv für das Lesen von env vars
+const {
+  getSessionFromStorage,
+  Session,
+} = require("@inrupt/solid-client-authn-node");
+import dotenv from "dotenv"; // Dotenv für das Lesen von env vars
+import { ok } from "assert";
+import cors from "cors";
 
 dotenv.config();
 
@@ -12,15 +17,16 @@ const app = express();
 const port = 8080;
 const session = new Session(); // Backend Session
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (_ : Request, res: Response) => {
+app.get("/", (_: Request, res: Response) => {
   res.send("Backend running!");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on ${port}`);
-  console.log(`http://localhost:${port}`)
+  console.log(`Server running on http://localhost:${port}`);
 });
 
 /**
@@ -31,54 +37,69 @@ app.get("/login", async (_: Request, res: Response) => {
     await sessionLogin();
     res.json({ success: true });
   } catch (error) {
-    res.json({ success: false, error: error instanceof Error ? error.message : String(error) });
+    res.json({
+      success: false,
+      error: error instanceof Error ? error.message : String(error),
+    });
   }
 });
 
 /**
  * Bekommt Abmeldungsbefehl vom Frontend.
  */
-app.get("/logout", async (req : Request, res : Response) => {
+app.get("/logout", async (_: Request, res: Response) => {
   try {
     await sessionLogout();
     res.json({ success: true });
   } catch (error) {
-    res.json({ success: false, error: error instanceof Error ? error.message : String(error) });
+    res.json({
+      success: false,
+      error: error instanceof Error ? error.message : String(error),
+    });
   }
-})
+});
 
-app.get("/save_adress", async (res : Response, req : Request) => {
-  
+app.get("/test", async (req: Request, res: Response) => {
+  console.log("gotcha!");
+  res.status(200).send("OK");
+});
+
+app.post("/save_address", async (req: Request, res: Response) => {
   const data = req.body;
   const WebID = data.WebID;
   const sourceURL = data.sourceURL;
-  const destinations = [...data.destinationURL];
+  const destinations = data.destinations;
+
+  console.log("WebID: ", WebID);
+  console.log("sourceURL: ", sourceURL);
+  console.log("destionations: ", destinations);
 
   // WriteToThirdParty(WebID, sourceURL, destinations) ???
-
-})
+  res.json({ status: "ok" });
+});
 
 async function sessionLogin() {
   try {
-
     const clientId = process.env.CLIENT_ID;
     const clientSecret = process.env.CLIENT_SECRET;
     const oidcIssuer = process.env.OIDC_ISSUER;
 
     if (!clientId || !clientSecret || !oidcIssuer) {
-      throw new Error('Missing environment variables CLIENT_ID, CLIENT_SECRET, or OIDC_ISSUER');
+      throw new Error(
+        "Missing environment variables CLIENT_ID, CLIENT_SECRET, or OIDC_ISSUER"
+      );
     }
 
     await session.login({
       clientId,
       clientSecret,
       oidcIssuer,
-      tokenType: 'client_secret'
+      tokenType: "client_secret",
     });
 
     if (session.info.isLoggedIn) {
       // You can change the fetched URL to a private resource, such as your Pod root.
-      if(session.info.webId) {
+      if (session.info.webId) {
         await session.fetch(session.info.webId);
         console.log("Session logged in successfully.");
       } else {
