b922517a5d8d90d500f531ba65276b245dad1905
@tomlonca <tomas_esteban.loncarica@stud.htwk-leipzig.de>
2026-01-18 12:27:35 +0100

    chore: refactor formatForms, um eines Liste [antrag_type, timestamp] einer gegebenen URLs Liste nach WebID zu extrahieren. Relates to #16

--- src/utils/formatForms.ts
+++ src/utils/formatForms.ts
@@ -1,14 +1,32 @@
+import { UserForms } from "../types.js";
+
 /**
  * Nimmt URLs und gibt einen neuen JSON Objekt zurück mit antrag_type und timestamp
  * @param urls Liste alles URLs, die man transformieren muss.
- * @returns JSON Objekt der Art 
+ * @returns JSON Objekt der Art
  * {
  *  forms {
  *    "antrag_type": string,
  *     "timestamp": string
  *  }[]
  * }
-*/
-export function formatForms(urls : string[]) /* : Promise<UserForms> */ {
- // Impl.
+ */
+export function formatForms(filenames: string[], webId: string): UserForms {
+  
+  const forms: { antrag_type: string; timestamp: string }[] = [];
+
+  filenames.map((f) => f.replace(/'/g, ""))
+    .forEach((f) => {
+      const match = f.match(/^antrag_(.+?)_([^_]+)_(\d+)\.ttl$/);
+      if (match && match[2] === webId) {
+        const form = {
+          antrag_type: match[1]!,
+          timestamp: match[3]!,
+        };
+        console.log("Gefunden:", form);
+        forms.push(form);
+      }
+    });
+
+  return { forms };
 }
